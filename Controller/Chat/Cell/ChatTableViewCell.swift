0b171f10fa15003d5ca13993f0206c0eacaff6db
Melodia <a2005062550322@gmail.com>
2025-08-26 12:49:45 +0800

    第二版-Main畫面更新

---
+++ Controller/Chat/Cell/ChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTableViewCell.swift
+//  Gemini
+//
+//  Created by 黃盈雅 on 2025/8/26.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
